f4de7fb59d79ac36f7c2c9386f3715eaa5129983
Alexander Wyss <alexsilvan.wyss@gmail.com>
2019-06-12 00:00:45 +0200

    game preperation for different task types and score

--- SolveOrDrinkIt/Controllers/GameHub.cs
+++ SolveOrDrinkIt/Controllers/GameHub.cs
@@ -12,6 +12,7 @@ namespace SolveOrDrinkIt.Controllers
 {
     public class GameHub : Hub
     {
+        private static Dictionary<string, ComputedTask> lastTasks = new Dictionary<string, ComputedTask>();
         private GameRepository repo = new GameRepository(new SolveOrDrinkItEntities());
 
         public Task JoinGame(string gameId)
@@ -19,17 +20,65 @@ namespace SolveOrDrinkIt.Controllers
             return Groups.Add(Context.ConnectionId, gameId);
         }
 
-        public void NextTask(string gameId)
+        public void GetTask(string gameId)
+        {
+            if (lastTasks.ContainsKey(gameId))
+            {
+                SendTask(gameId, lastTasks[gameId]);
+            }
+            else
+            {
+                NextTask(gameId);
+            }
+        }
+
+        public void Drink(string gameId)
+        {
+            //TODO add score
+            NextTask(gameId);
+        }
+
+        public void Completed(string gameId)
+        {
+           NextTask(gameId);
+        }
+
+        private void NextTask(string gameId)
         {
             List<Models.Task> tasks = repo.Get(gameId.AsInt()).Deck.Tasks.ToList();
-            string task = tasks[new Random().Next(tasks.Count)].text;
-            Clients.Group(gameId).newTask(task);
-
+            ComputedTask task  = new ComputedTask()
+            {
+                Task = tasks[new Random().Next(tasks.Count)]
+            };
+            lastTasks[gameId] = task;
+            SendTask(gameId, task);
         }
+
+        private void SendTask(string gameId, ComputedTask task)
+        {
+            Clients.Group(gameId).newTask(ShowCompletedButton(task), RenderText(task));
+        }
+
+        private bool ShowCompletedButton(ComputedTask task)
+        {
+            return task.Task.type == 0; //TODO
+        }
+
+        private string RenderText(ComputedTask task)
+        {
+            return task.Task.text; //TODO
+        }
+
         public void EndGame(string gameId)
         {
             GamesController.currentGames.Remove(gameId.AsInt());
+            lastTasks.Remove(gameId);
             Clients.Group(gameId).endGame();
         }
+    }
+
+    public class ComputedTask
+    {
+        public Models.Task Task { get; set; }
     }
 }
